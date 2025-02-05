4e9c501d14dcc262a3801ef7dde7942649e4143d
Filip Kestner <filip.kestner@student.kdg.be>
2025-02-05 15:24:06 +0100

    init

---
+++ MatcherContext/src/main/java/be/kdg/prog6/MatcherContext/MatcherContextApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.prog6.MatcherContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatcherContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatcherContextApplication.class, args);
+    }
+
+}
