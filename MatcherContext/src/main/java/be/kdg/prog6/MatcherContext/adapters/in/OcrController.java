63d12589e1dfb7dbeabfc51d6ecbe154f3092158
Opeyemi Momodu <opeyemimomodu2016@gmail.com>
2025-02-17 00:00:40 +0100

    Add logging for OCR request and result in OcrController.

--- MatcherContext/src/main/java/be/kdg/prog6/MatcherContext/adapters/in/OcrController.java
+++ MatcherContext/src/main/java/be/kdg/prog6/MatcherContext/adapters/in/OcrController.java
@@ -3,6 +3,8 @@ package be.kdg.prog6.MatcherContext.adapters.in;
 import be.kdg.prog6.MatcherContext.domain.ImageData;
 import be.kdg.prog6.MatcherContext.domain.OcrResult;
 import be.kdg.prog6.MatcherContext.ports.in.OcrUseCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +17,11 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/api/ocr")
 public class OcrController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OcrController.class);
+
     private final OcrUseCase ocrUseCase;
+
 
     public OcrController(OcrUseCase ocrUseCase) {
         this.ocrUseCase = ocrUseCase;
@@ -23,8 +29,13 @@ public class OcrController {
 
     @PostMapping
     public ResponseEntity<OcrResult> performOcr(@RequestParam("file") MultipartFile file) throws IOException {
+        logger.info("Received OCR request with file: {}", file.getOriginalFilename());
+
         ImageData imageData = new ImageData(file.getBytes());
         OcrResult result = ocrUseCase.performOcr(imageData);
+
+        logger.info("OCR result: {}", result);
+
         return ResponseEntity.ok(result);
     }
 }
