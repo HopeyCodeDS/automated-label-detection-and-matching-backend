44c2d68fc90f39fe2413fda69b5ff82f122754d5
Opeyemi Momodu <opeyemimomodu2016@gmail.com>
2025-02-17 13:59:57 +0100

    Update application properties, add CommandLineRunner to CompleteApplication, and modify the structure of the data.sql file to include additional fields.

--- application/src/main/java/be/kdg/prog6/CompleteApplication.java
+++ application/src/main/java/be/kdg/prog6/CompleteApplication.java
@@ -1,18 +1,30 @@
 package be.kdg.prog6;
 
 
+import be.kdg.prog6.MatcherContext.adapters.out.CsvDataLoader;
 import org.opencv.core.Core;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
         "be.kdg.prog6"
 })
-public class CompleteApplication {
+public class CompleteApplication implements CommandLineRunner {
+
+    @Autowired
+    private CsvDataLoader csvDataLoader;
+
     public static void main(String[] args) {
         SpringApplication.run(CompleteApplication.class, args);
 
         // Load OpenCV
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        csvDataLoader.loadData();
     }
 }
